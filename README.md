965ab08257ac66bc54cbee5a45a1547276753511
coldboy002 <coldboy002@gmail.com>
2020-03-14 09:36:22 +0700

    feat(): get and post http use axios

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
 ## TODOLIST APP
-### Feature:
+## Feature:
 - add todo
 - remove todo
+- edit todo
+## Getting started
+### Json-server:
+1. Install Node.js
+2. Install JSON Server
+```md
+npm install -g json-server
+```
+3. Create a db.json file with some data
+```json
+{
+  "todos": [
+    {
+      "id": 1,
+      "content": "Go to bed"
+    },
+    {
+      "id": 2,
+      "content": "Watch TV"
+    },
+    {
+      "id": 3,
+      "content": "Read book"
+    }
+  ]
+}
+```
+4. Start JSON Server
+```md
+json-server --watch db.json
+```
+
+### browsersync 
+1. Install Node.js
+2. Install Browsersync
+```md
+npm install -g browser-sync
+```
+3. Start Browsersync
+```md
+browser-sync start --server --files "*.css"
+```
--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
   let htmlList = document.getElementById("todo-list");
-  let addBtn = document.getElementById("add-btn");
-  let input = document.getElementById("new-item");
-  // key localStorage
-  const storageKey = "TODOS";
-  //get localStorage with key is variable storageKey if null assign todo list is empty array ([]);
-  let todoList = JSON.parse(localStorage.getItem(storageKey)) || [];
   let context;
-  // add todo
-  addBtn.addEventListener("click", addItem);
-  input.value = sessionStorage.getItem("draft");
-  input.addEventListener("change", setSessionStorage);
-
-  function addItem() {
-    let newItem = input.value;
-    if (newItem) {
-      todoList.push(newItem);
-    }
-    render();
-    // set localStorage with key is variable storageKey
-    localStorage.setItem(storageKey, JSON.stringify(todoList));
-    input.value = "";
-  }
-  function setSessionStorage(e) {
-    sessionStorage.setItem("draft", e.target.value);
-  }
+  const url = "http://localhost:3000/todos";
+  axios
+    .get(url)
+    .then(function(res) {
+      // handle success
+      render(res.data);
+    })
+    .catch(function(err) {
+      // handle error
+      console.log(err);
+    });
   // render HTML
-  function render() {
-    context = todoList.map(item => `<li>${item}</li>`);
+  function render(items) {
+    context = items.map(item => `<li>${item.content}</li>`);
     htmlList.innerHTML = context.join("");
   }
-
-  render();
 });
